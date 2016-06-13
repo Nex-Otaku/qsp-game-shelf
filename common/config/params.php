bc3c4855daae99a905e3d9e25d6c3b66c02300e3
Nex Otaku <nex@otaku.ru>
2016-06-13 21:46:06 +0300

    Обновил настройки сайта.

--- common/config/params.php
+++ common/config/params.php
@@ -1,9 +1,9 @@
 <?php
 return [
     // Компания - разработчик сайта.
-    'developerCompanyName' => 'KAMOD',
-    'developerCompanySiteTitle' => 'KAMOD.PRO',
-    'developerCompanySiteUrl' => 'http://kamod.pro',
+    'developerCompanyName' => 'Nex Otaku',
+    'developerCompanySiteTitle' => 'Butterfly Lantern',
+    'developerCompanySiteUrl' => 'http://butterfly-lantern.ru',
     // STUB поменять перед релизом адрес почтового робота.
     'robotEmail' => 'robot@myhost.ru',
     'user.passwordResetTokenExpire' => 3600,
@@ -11,6 +11,6 @@ return [
     // Позже нужно будет поменять на "upload", 
     // чтобы в корне не было множества папок с пользовательскими файлами.
     'lib' => '/media_library/',
-    'site_title' => 'My Cool Site',
-    'site_title_for_tab' => 'My Cool Site',
+    'site_title' => 'Текстовые игры',
+    'site_title_for_tab' => 'Текстовые игры',
 ];
