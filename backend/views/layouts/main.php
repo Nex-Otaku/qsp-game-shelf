e9031e0d02f1407691466c812295febed12fcafd
Nex Otaku <nex@otaku.ru>
2016-08-09 08:20:59 +0300

    Убрал лишнее, причесал админку.

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -30,6 +30,7 @@ if (Yii::$app->controller->action->id === 'login') {
     <!DOCTYPE html>
     <html lang="<?= Yii::$app->language ?>">
     <head>
+        <meta name="robots" content="nofollow" />
         <meta charset="<?= Yii::$app->charset ?>"/>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <?= Html::csrfMetaTags() ?>
