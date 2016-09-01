96afac5f149c4a3e08784b0a7c6223c069a8138a
Nex Otaku <nex@otaku.ru>
2016-09-01 09:04:52 +0300

    Поправил скрипт редактирования.

--- backend/views/feed/update.php
+++ backend/views/feed/update.php
@@ -7,7 +7,7 @@ use yii\helpers\Html;
 
 $this->title = Yii::t('app', 'Update {modelClass}: ', [
    'modelClass' => 'Feed',
-]) . $model->id;
+]) . $model->slug;
 $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Feeds'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = Yii::t('app', 'Update');
