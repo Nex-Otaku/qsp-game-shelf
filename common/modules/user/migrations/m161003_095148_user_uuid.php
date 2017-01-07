f142b68d1e848435773c1f4ce2959e13a47835d7
Nex Otaku <nex@otaku.ru>
2017-01-07 11:37:39 +0300

    Добавил комментарий.

--- common/modules/user/migrations/m161003_095148_user_uuid.php
+++ common/modules/user/migrations/m161003_095148_user_uuid.php
@@ -4,6 +4,15 @@ use dmstr\db\mysql\FileMigration;
 
 class m161003_095148_user_uuid extends FileMigration
 {
-    # create a sql file `m161003_095148_user_uuid.sql` or adjust and uncomment the following line, do not change this class name
-    //public $file = 'custom-filename.sql';
+    // TODO
+    // Переделать миграцию.
+    // Сейчас, миграции идут в таком порядке:
+    // 1. Миграция модуля "dektrium/yii2-user"
+    // 2. Миграция модуля "mdmsoft/yi2-admin"
+    // 3. Миграция модуля "nex-otaku/yii2-uuid-user".
+    // 
+    // Из-за этого, модули "nex-otaku/yii2-uuid-user" и "nex-otaku/yii2-uuid-admin" слишком плотно связаны.
+    // 
+    // Нужно сделать самостоятельные миграции для "nex-otaku/yii2-uuid-user" и "nex-otaku/yii2-uuid-admin",
+    // чтобы они могли сразу создавать таблицы с бинарными полями, каждый для своих нужд. 
 }
