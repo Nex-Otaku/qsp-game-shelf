92084fff0da4dbe772e411ef37f0d6a6cecbb70b
Nex Otaku <nex@otaku.ru>
2017-01-26 07:20:50 +0300

    Перевёл поля в бинарный формат, добавил поля для SoftDelete.

---
+++ console/migrations/m170126_041118_table_feed_add_columns_for_softdelete.php
@@ -0,0 +1,9 @@
+<?php
+
+use dmstr\db\mysql\FileMigration;
+
+class m170126_041118_table_feed_add_columns_for_softdelete extends FileMigration
+{
+    # create a sql file `m170126_041118_table_feed_add_columns_for_softdelete2.sql` or adjust and uncomment the following line, do not change this class name
+    //public $file = 'custom-filename.sql';
+}
---
+++ console/migrations/m170126_041500_table_feed_change_fields_to_binary.php
@@ -0,0 +1,9 @@
+<?php
+
+use dmstr\db\mysql\FileMigration;
+
+class m170126_041500_table_feed_change_fields_to_binary extends FileMigration
+{
+    # create a sql file `m170126_041500_table_feed_change_fields_to_binary.sql` or adjust and uncomment the following line, do not change this class name
+    //public $file = 'custom-filename.sql';
+}
