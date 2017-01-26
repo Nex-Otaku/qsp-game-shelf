eb6654aa2ec0da1c88a39f82077b19f3b94574d9
Nex Otaku <nex@otaku.ru>
2017-01-26 07:20:02 +0300

    Подключил генератор миграций.

--- console/config/main.php
+++ console/config/main.php
@@ -21,6 +21,19 @@ return [
             ],
         ],
     ],
+    'controllerMap' => [
+        'migrate' => [
+            'class' => 'yii\console\controllers\MigrateController',
+            'templateFile' => '@vendor/dmstr/yii2-db/db/mysql/templates/file-migration.php',
+            'generatorTemplateFiles' => [
+                'create_table' => '@vendor/dmstr/yii2-db/db/mysql/templates/file-migration.php',
+                'drop_table' => '@vendor/dmstr/yii2-db/db/mysql/templates/file-migration.php',
+                'add_column' => '@vendor/dmstr/yii2-db/db/mysql/templates/file-migration.php',
+                'drop_column' => '@vendor/dmstr/yii2-db/db/mysql/templates/file-migration.php',
+                'create_junction' => '@vendor/dmstr/yii2-db/db/mysql/templates/file-migration.php',
+            ],
+        ],
+    ],
     'modules' => [
         'user' => [
             'class' => 'dektrium\user\Module',
