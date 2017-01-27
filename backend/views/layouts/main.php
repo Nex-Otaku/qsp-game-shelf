ca417e93b69439c1af7045ba5a13ad1c4fbe165f
Nex Otaku <nex@otaku.ru>
2017-01-27 09:35:38 +0300

    Исправил страницу регистрации.

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -5,7 +5,8 @@ use yii\helpers\Html;
 /* @var $content string */
 
 
-if (Yii::$app->controller->action->id === 'login') { 
+$action = Yii::$app->controller->action->id;
+if (($action === 'login') || ($action === 'register')) { 
 /**
  * Do not use this code in your template. Remove it. 
  * Instead, use the code  $this->layout = '//main-login'; in your controller.
