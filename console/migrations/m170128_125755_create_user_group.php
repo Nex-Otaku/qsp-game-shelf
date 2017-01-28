301e0f621a696533fdbbda5cb7af24e813b3ef07
Nex Otaku <nex@otaku.ru>
2017-01-28 18:34:02 +0300

    Миграции для групп пользователей.

---
+++ console/migrations/m170128_125755_create_user_group.php
@@ -0,0 +1,9 @@
+<?php
+
+use dmstr\db\mysql\FileMigration;
+
+class m170128_125755_create_user_group extends FileMigration
+{
+    # create a sql file `m170128_125755_create_user_group.sql` or adjust and uncomment the following line, do not change this class name
+    //public $file = 'custom-filename.sql';
+}
